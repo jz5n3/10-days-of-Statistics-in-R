52788a37da80af29e32d71aecc4e82f805c94caa
jz5n3 <shirly.sky@hotmail.com>
2018-04-13 17:08:11 -0500

    Update day0.R

--- day0.R
+++ day0.R
@@ -1,9 +1,14 @@
 # Day 0: Mean, Median, Mode
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 stat<-function(x){data=sort(x)
-                  modes=names(table(data))[table(data)==max(table(data))]
+                 modes=names(table(data))[table(data)==max(table(data))]
                   m3=modes[1]
                   return(m3)}
-STDIN<-sort(c(64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060))
+x<-file('stdin', open='r')
+y<-readLines(x)[[2]]
+z<-strsplit(y," ")
+STDIN<-as.numeric(unlist(z))
+
 m3=stat(STDIN)
 m1=mean(STDIN)
 m2=median(STDIN)
@@ -11,3 +16,4 @@ m2=median(STDIN)
 cat(m1,sep="\n")
 cat(m2,sep="\n")
 cat(m3,sep="\n")
+
