2ba0428bf8ac92608ca372dcb6f74c90ee835cd6
jz5n3 <shirly.sky@hotmail.com>
2018-04-15 00:43:36 -0500

    Update day1.R

--- day1.R
+++ day1.R
@@ -17,3 +17,27 @@ quantile<-function(x){
 }
 r<-quantile(data)
 cat(r,sep="\n")
+
+# Interquartile Range
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+input<-file('stdin',open='r')
+input<-readLines(input)
+e<-as.numeric(unlist(strsplit(input[2]," ")))
+f<-as.numeric(unlist(strsplit(input[3]," ")))
+s<-rep(e,f)
+IQR<-function(x){
+    y<-sort(x)
+    n<-length(y)
+    Q2<-(n+1)/2
+    if(floor(Q2)==Q2){
+        Q1=Q2-1
+        Q3=Q2+1        
+    }else{
+        Q1=Q2-0.5
+        Q3=Q2+0.5
+    }
+    IQR<-median(y[Q3:n])-median(y[1:Q1])
+    return(IQR)
+}
+calIQR<-format(round(IQR(s),1),nsmall=1)
+cat(calIQR,sep="\n")
