7e616977a0fcfdbf70fd9b0939af6b835d65c34b
jz5n3 <shirly.sky@hotmail.com>
2018-04-17 13:06:55 -0500

    Update day1.R

--- day1.R
+++ day1.R
@@ -55,4 +55,22 @@ squareroot<-function(x){
 sqr<-squareroot(data)
 cat(format(round(sqr,1),nsmall=1),sep="/n")
 
+# Cummulative Probability
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+boy <- 1.09/2.09
+biDistr<-function(n,x,p){
+    result<-(factorial(n)/(factorial(x)*factorial(n-x)))*(p^x)*(1-p)^(n-x)
+    return(result)
+}
+result<-function(n,x,p){
+    result<-0
+    for (i in x:n){
+        result=result+biDistr(n,i,p)
+    }
+    return(result)
+}
 
+CumPro<-result(6,3,boy)
+
+
+cat(round(CumPro,3),sep="\n")
