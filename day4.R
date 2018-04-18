0876eb0a310612f2341f94e26e104290f1e1a145
jz5n3 <shirly.sky@hotmail.com>
2018-04-17 23:23:54 -0500

    Update day4.R

--- day4.R
+++ day4.R
@@ -1,4 +1,4 @@
-# Cummulative Probability
+# Binomial Distribution I
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 boy <- 1.09/2.09
 biDistr<-function(n,x,p){
@@ -17,3 +17,21 @@ CumPro<-result(6,3,boy)
 
 
 cat(round(CumPro,3),sep="\n")
+
+#Binomial Distribution II
+p<-0.12
+binDistr<-function(n,x,p){
+    dist<-(factorial(n)/(factorial(x)*factorial(n-x)))*(p^x)*(1-p)^(n-x)
+    return(dist)
+}
+cumdistr<-function(n,x,p){
+    result<-0
+    for (i in 0:x){
+        result=result+binDistr(n,i,p)
+    }
+    return(result)
+}
+q1<-cumdistr(10,2,p)
+q2<-1-q1+binDistr(10,2,p)
+cat(round(q1,3),sep="\n")
+cat(round(q2,3),sep="\n")
