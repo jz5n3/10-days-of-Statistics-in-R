c98a37cd591925575d2c7b248105e78766645a8e
jz5n3 <shirly.sky@hotmail.com>
2018-04-19 22:40:16 -0500

    Update day6.R

--- day6.R
+++ day6.R
@@ -1,4 +1,9 @@
-# Central Limit Theorem
+# Central Limit Theorem I
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 result<-pnorm(9800,mean=205*49,sd=15*sqrt(49))
+cat(round(result,4),sep="\n")
+
+# Central Limit Theorem II
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+result<-pnorm(250,mean=2.4*100,sd=2*sqrt(100))
 cat(round(result,4),sep="\n")
